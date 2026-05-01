958677eca02d2367d97d9650fdc80c61636d5138
Camilo Castellanos <kml107@hotmail.com>
2026-05-01 16:09:11 -0500

    Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import NavBar  from './app/ui/components/navbar.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar  from './app/ui/components/NavBar.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export function App() {
   const [count, setCount] = useState(0)
